de71953bb54efd7fe599e72d879f30f53bbcb927
HQs <409337645@qq.com>
2026-02-25 15:53:00 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { Button } from "@/components/ui/button";
+
+const router = useRouter();
+
+const goToDetail = () => {
+  router.push("/detail");
+};
+</script>
+
+<template>
+  <div>
+    <Button variant="outline" @click="goToDetail">进入详情页</Button>
+  </div>
+</template>
+
+<style scoped></style>
